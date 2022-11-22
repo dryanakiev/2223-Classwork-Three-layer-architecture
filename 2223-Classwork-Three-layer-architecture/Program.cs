12c53805244631d6fc179026a0a63afb0c9315fa
Димитър Янакиев <dyanakiev@codingburgas.bg>
2022-11-22 12:25:50 +0200

    Add project files.

---
+++ 2223-Classwork-Three-layer-architecture/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace _2223_Classwork_Three_layer_architecture
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
